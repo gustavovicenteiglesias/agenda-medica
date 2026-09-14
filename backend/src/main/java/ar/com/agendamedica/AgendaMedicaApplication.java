412445fb27e110b542d744ae876e2fd123fafd69
Gustavo V Iglesias <gustavo.iglesias40@gmail.com>
2026-09-14 19:04:28 -0300

    feat(backend): add Spring Boot application entry point

---
+++ backend/src/main/java/ar/com/agendamedica/AgendaMedicaApplication.java
@@ -0,0 +1,11 @@
+package ar.com.agendamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaMedicaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaMedicaApplication.class, args);
+    }
+}
